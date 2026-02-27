15f1f73dd239bc60678542e04b2a6814f3cf432d
Quyen <quyen@example.com>
2026-02-27 16:21:54 +0700

    Add backend (Strapi) files to monorepo

---
+++ backend/config/middlewares.ts
@@ -0,0 +1,27 @@
+import type { Core } from '@strapi/strapi';
+
+const config: Core.Config.Middlewares = [
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'http://localhost:3000',
+        'http://localhost:3001',
+        'https://phapmontamlinh.vn',
+        'https://www.phapmontamlinh.vn',
+      ],
+      credentials: true,
+    },
+  },
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
+
+export default config;
